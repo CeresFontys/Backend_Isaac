b9b4a65ed7d4becbd9d9f22a2c948378e0cf3a63
Dylan Nas <KurzaCationer@gmail.com>
2020-10-12 23:40:52 +0200

    Feedback verwerkt en bugs gefixt. Er waren een aantal stijl problemen.

--- Structurizr/Program.cs
+++ Structurizr/Program.cs
@@ -24,41 +24,88 @@ namespace Structurizr
             user.Uses(softwareSystem, "Uses");
             admin.Uses(softwareSystem, "Uses");
             
-            //External Software Systems
-            var mqtt = model.AddSoftwareSystem("MQTT", "pub/sub Event bus");
-            softwareSystem.Uses(mqtt, "Uses");
-            
-            var mysql = model.AddSoftwareSystem("MySQL", "Business database");
-            softwareSystem.Uses(mysql, "Uses");
-            
-            var influx = model.AddSoftwareSystem("InfluxDB", "Sensor time series database");
-            softwareSystem.Uses(influx, "Uses");
-            
+            SoftwareSystem externalSensorService = model.AddSoftwareSystem(Location.External,"Isaac sensor service", "MQTT Input data");
+            softwareSystem.Uses(externalSensorService, "Receives Data");
+            //Internal Software Containers
+            var mqtt = softwareSystem.AddContainer("Event Bus", "pub/sub Event Bus", "MQTT");
+            mqtt.Uses(externalSensorService, "Receives Data");
+            mqtt.AddTags(new []{"Event"});
+            var mysql = softwareSystem.AddContainer("Business Database", "Business database", "MySQL");
+            mysql.AddTags(new []{"Database"});
+            var influx = softwareSystem.AddContainer("Sensor Database", "Sensor time series database", "InfluxDB");
+            influx.AddTags(new []{"Database"});
+
             //Containers of the software system
             var frontend = softwareSystem.AddContainer("SPA Frontend", "The frontend of the application", "React.js (hosted by nginx)");
-            visitor.Uses(frontend, "Uses");
-            user.Uses(frontend, "Uses");
-            admin.Uses(frontend, "Uses");
+            visitor.Uses(frontend, "Interacts");
+            user.Uses(frontend, "Interacts");
+            admin.Uses(frontend, "Manages");
 
-            frontend.Uses(mqtt, "Uses");
+            frontend.Uses(mqtt, "Receives Data");
             
             var backend = softwareSystem.AddContainer("API Backend","The backend of the application", "ASP.NET Core");
             frontend.Uses(backend, "Uses");
             
-            backend.Uses(mqtt, "Uses");
-            backend.Uses(mysql, "Uses");
-            backend.Uses(influx, "Uses");
+            backend.Uses(mqtt, "Processes Data");
+            mqtt.Uses(backend, "Receives Data");
+            backend.Uses(mysql, "Stores Information");
+            backend.Uses(influx, "Stores Data");
             
             //Frontend components
             //frontend.AddComponent("", "", "", "");
             
             
             //Backend components
-            //backend.AddComponent("", "", "", "");
+            var accountService = backend.AddComponent("Account Service", "This component handles account interactions", "ASP.NET Core API Controller");
+            accountService.Uses(mysql, "Stores Data");
+            var authorizationService = backend.AddComponent("Authorization Service", "This component handles permissions", "ASP.NET Core API Controller");
+            authorizationService.Uses(mysql, "Stores Data");
+            var anomalyService = backend.AddComponent("Sensor Anomaly Service", "this component detects anomalies and logs them", "ASP.NET Core API Controller");
+            anomalyService.Uses(mqtt, "Processed Data");
+            anomalyService.Uses(mysql, "Stores Errors");
+            var settingsService = backend.AddComponent("Sensor Settings Service", "this components managing changing settings related to sensors", "ASP.NET Core API Controller");
+            settingsService.Uses(influx, "Edits preferences");
+            var dataService = backend.AddComponent("Data Management", "This extracts mqtt data and populates the influxdb and then repopulates mqtt", "ASP.NET Core API Controller");
+            dataService.Uses(mqtt, "Processes Data");
+            dataService.Uses(influx, "Stores Data");
+
+            frontend.Uses(accountService, "");
+            frontend.Uses(authorizationService, "");
+            frontend.Uses(anomalyService, "");
+            frontend.Uses(settingsService, "");
+            frontend.Uses(dataService, "");
+
+            mqtt.Uses(dataService, "Receives Data");
+
+
+            //Styles
+            ViewSet viewSet = workspace.Views;
+            Styles styles = viewSet.Configuration.Styles;
+
+            var softwareSystemStyle = new ElementStyle(Tags.SoftwareSystem);
+            softwareSystemStyle.Background = "#9bbede";
+            softwareSystemStyle.Color = "#ffffff";
+
+            var personStyle = new ElementStyle(Tags.Person);
+            personStyle.Background = "#08427b";
+            personStyle.Color = "#ffffff";
+            personStyle.Shape = Shape.Person;
+            
+            var databaseStyle = new ElementStyle("Database");
+            databaseStyle.Background = "#7aaede";
+            databaseStyle.Shape = Shape.Cylinder;
+            
+            var eventStyle = new ElementStyle("Event");
+            eventStyle.Background = "#9bbede";
+            eventStyle.Shape = Shape.Cylinder;
+
+            viewSet.Configuration.Styles.Add(softwareSystemStyle);
+            viewSet.Configuration.Styles.Add(personStyle);
+            viewSet.Configuration.Styles.Add(databaseStyle);
+            viewSet.Configuration.Styles.Add(eventStyle);
             
             //Views
-            ViewSet viewSet = workspace.Views;
-            
+
             SystemContextView contextView = viewSet.CreateSystemContextView(softwareSystem, "SystemContext", "The System Context diagram");
             contextView.AddAllSoftwareSystems();
             contextView.AddAllPeople();
@@ -67,33 +114,17 @@ namespace Structurizr
             containerView.AddAllPeople();
             containerView.AddAllContainers();
             containerView.AddAllSoftwareSystems();
-            
-            /*
-            ComponentView componentView =
-                viewSet.CreateComponentView(container, "Component", "A Component Diagram (of an Container)");
-            componentView.AddNearestNeighbours(container);
-            foreach (Component comp in container)
+
+
+            ComponentView backendView =
+                viewSet.CreateComponentView(backend, "Component", "The Backend Diagram");
+            backendView.AddNearestNeighbours(backend);
+            foreach (Component comp in backend.Components)
             {
-                componentView.Add(comp);
+                backendView.Add(comp);
             }
-            */
-            
-            //Styles
-            Styles styles = viewSet.Configuration.Styles;
 
-            var elementStyles = new List<ElementStyle>();
-            
-            var softwareSystemStyle = new ElementStyle(Tags.SoftwareSystem);
-            softwareSystemStyle.Background = "#1168bd";
-            softwareSystemStyle.Color = "#ffffff";
-
-            var personStyle = new ElementStyle(Tags.Person);
-            personStyle.Background = "#08427b";
-            personStyle.Color = "#ffffff";
-            personStyle.Shape = Shape.Person;
-
-            styles.Elements.Add(softwareSystemStyle);
-            styles.Elements.Add(personStyle);
+            backendView.Remove(backend);
             
             //Upload
             StructurizrClient structurizrClient = new StructurizrClient(" 8afe380d-d3a2-46a5-9f24-0aa290cb2455", "2219e66c-d2d3-4a6a-bb84-c9700f15d77c");
