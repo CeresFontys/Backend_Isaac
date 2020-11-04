31817c9a4bc8e8ec1b6ca963157386874d1c5d18
Emirhan <Demirci.Emirhan@outlook.com>
2020-11-04 10:32:46 +0100

    Authenticate service done

---
+++ Isaac-AuthorizationService/Models/JwtUser.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Isaac_AuthorizationService.Models
+{
+    [NotMapped]
+    public class JwtUser
+    {
+        public User User { get; set; }
+        public string Token { get; set; }
+
+    }
+}
---
+++ Isaac-AuthorizationService/Services/AuthService.cs
@@ -0,0 +1,33 @@
+﻿using Isaac_AuthorizationService.Data;
+using Isaac_AuthorizationService.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Isaac_AuthorizationService.Services
+{
+    public class AuthService
+    {
+        private readonly ApplicationDbContext _dbContext;
+        public AuthService(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        public JwtUser Authenticate(string username, string password)
+        {
+            var jwtKey = "TryToGuessThisPassword";
+            var user = _dbContext.Users.FirstOrDefault(u => u.Username == username);
+
+            if (user == null || password != user.Password)
+            {
+                return null;
+            }
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+
+            }
+        }
+    }
+}
