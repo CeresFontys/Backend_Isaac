fadf7b2fbc3e6b483f6742be91e1aca0e74bf7a4
DavidRRg <57133584+DavidRRg@users.noreply.github.com>
2020-11-11 09:22:13 +0100

    Updated settingsModel

--- Isaac-SensorSettingService/Models/SettingsModel.cs
+++ Isaac-SensorSettingService/Models/SettingsModel.cs
@@ -1,16 +1,18 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
+using CsvHelper.Configuration.Attributes;
 
 namespace Isaac_SensorSettingService.Models
 {
     [Table("SensorSettings")]
     public class SettingsModel
     {
+        [Key]
         public int Id  { get; set; }
-        public int Floor_id { get; set; }
         public int RefreshRate { get; set; }
         public bool KeepData { get; set; }
         public int ExpirationTime { get; set; }
